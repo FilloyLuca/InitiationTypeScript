9c71d3a3ba97206abb7947b7695ce6aceb9837a5
FilloyLuca <filloylucapro@gmail.com>
2025-12-04 10:00:08 +0100

    test de déclarations et d'initialisations

---
+++ ScriptsTS/build/test.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// le ; n'est pas obligatoire 
+// Déclaration sans initialisation 
+let nombre;
+let texte;
+let varlog;
+// Affectations des valeurs
+nombre = 33.7;
+texte = "Typescript est un langage avec un typage statique";
+varlog = true;
+// Vérification des types 
+console.log(`Type de nombre -> ${typeof (nombre)}`);
+console.log(`Type de texte -> ${typeof (texte)}`);
+console.log(`Type de varlog -> ${typeof (varlog)}`);
+//# sourceMappingURL=test.js.map
---
+++ ScriptsTS/src/test.ts
@@ -0,0 +1,15 @@
+// le ; n'est pas obligatoire 
+// Déclaration sans initialisation 
+let nombre: number;
+let texte: string;
+let varlog: boolean;
+
+// Affectations des valeurs
+nombre = 33.7; 
+texte = "Typescript est un langage avec un typage statique";
+varlog = true
+
+// Vérification des types 
+console.log(`Type de nombre -> ${typeof(nombre)}`);
+console.log(`Type de texte -> ${typeof(texte)}`);
+console.log(`Type de varlog -> ${typeof(varlog)}`);
